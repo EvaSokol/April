39f51030b50b67da0192d045c1a9009711430091
MASK <mask@pisem.net>
2018-06-15 22:44:30 +0300

    Create set of ideas added to liquibase

--- src/main/java/com/april/testproject/entity/Idea.java
+++ src/main/java/com/april/testproject/entity/Idea.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
 import org.hibernate.validator.constraints.NotEmpty;
-
 import javax.persistence.*;
 import java.util.Date;
 import java.util.Comparator;
@@ -56,78 +55,5 @@ public class Idea implements Comparator<Idea> {
 	public int compare(Idea i1, Idea i2) {
 		return i1.creationDate.compareTo(i2.creationDate);
 	}
-
-//	public String getTags() {
-//		return tags;
-//	}
-//
-//	public void setTags(String tags) {
-//		this.tags = tags;
-//	}
-//
-//	public String getHeader() {
-//		return header;
-//	}
-//
-//	public void setHeader(String header) {
-//		this.header = header;
-//	}
-//
-//	public String getMainPicture() {
-//		return mainPicture;
-//	}
-//
-//	public void setMainPicture(String mainPicture) {
-//		this.mainPicture = mainPicture;
-//	}
-//
-//	public String getFullDescription() {
-//		return fullDescription;
-//	}
-//
-//	public void setFullDescription(String fullDescription) {
-//		this.fullDescription = fullDescription;
-//	}
-//
-//	public String getPictureList() {
-//		return pictureList;
-//	}
-//
-//	public void setPictureList(String pictureList) {
-//		this.pictureList = pictureList;
-//	}
-//
-//	public Long getId() {
-//		return id;
-//	}
-//
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//
-//	public String getShortDescription() {
-//		return shortDescription;
-//	}
-//
-//	public void setShortDescription(String shortDescription) {
-//		this.shortDescription = shortDescription;
-//	}
-//
-//	public String getStatus() {
-//		return status;
-//	}
-//
-//	public void setStatus(String status) {
-//		this.status = status;
-//	}
-//
-//	public String getUserId() {
-//		return userId;
-//	}
-//
-//	public void setUserId(String userId) {
-//		this.userId = userId;
-//	}
-
 
 }
