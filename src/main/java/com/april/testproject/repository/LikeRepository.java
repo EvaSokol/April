36633675e8f88e369344c437a50107d5ae258109
MASK <mask@pisem.net>
2018-06-21 09:27:04 +0300

    Added: dislike

--- src/main/java/com/april/testproject/repository/LikeRepository.java
+++ src/main/java/com/april/testproject/repository/LikeRepository.java
@@ -1,5 +1,6 @@
 package com.april.testproject.repository;
 
+import com.april.testproject.entity.Idea;
 import com.april.testproject.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -20,5 +21,6 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
 	@Query(value = "SELECT DISTINCT l.ideaId FROM Like l")
 	List<Long> getLikedIdeas();
 
-
+	@Query(value = "SELECT l FROM Like l WHERE l.userId = ?1 AND l.ideaId = ?2")
+	List<Like> findLike(Long userId, Long ideaId);
 }
